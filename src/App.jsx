d036324e5b03b1d058280fefac15efe00a9e0913
khalid14414 <qwekukhalid266@gmail.com>
2025-05-30 01:51:39 +0000

    Modification of the folders and file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-
+import MainLayout  from "./layouts/MainLayout"
 function App() {
   
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline bg-emerald-200 mt-4">Welcome to the React App</h1>
-      <p className='text-lg text-gray-700 mt-8'>This is a simple React application using Tailwind CSS for styling.</p>
+      <MainLayout>
+        <main>
+         Main content goes here
+        </main>
+      </MainLayout>
     </>
   )
 }
