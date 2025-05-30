7e0c4a954c3a566daea9a4dcf3076ddc93f0c5da
khalid14414 <qwekukhalid266@gmail.com>
2025-05-30 01:29:49 +0000

    frist comment

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline bg-emerald-200 mt-4">Welcome to the React App</h1>
+      <p className='text-lg text-gray-700 mt-8'>This is a simple React application using Tailwind CSS for styling.</p>
+    </>
+  )
+}
+
+export default App
