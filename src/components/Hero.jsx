99d96e5ecb68ce57e979ea330f1fc8463a78124a
khalid14414 <qwekukhalid266@gmail.com>
2025-06-03 00:56:58 +0000

    Added hero component to the home page

---
+++ src/components/Hero.jsx
@@ -0,0 +1,29 @@
+function Hero() {
+    const link = "bg-green-500 py-4";
+
+    return (
+        <section className="bg-green-50 py-16">
+            <div className="max-w-7xl mx-auto px-6 grid grid-cols-1 md:grid-cols-2 gap-10 items-center" >
+                <div>
+                    <h1 className="text-4xl md:text-5xl font-bold text-green-800 leading-tight">
+                        Organic Mosquito Repellents for a Healthier Life
+                    </h1>
+                    <p className="mt-4 text-lg text-gray-700 max-w-md">
+                        Protect your family with our eco-friendly, safe, and organic solutions made from lemongrass, cloves, scent leaves, and more.
+                    </p>
+
+                    <div className="mt-6 flex flex-col sm:flex-row gap-4">
+                        <a href="http://">
+                            <button className="bg-green-700 text-white px-6 py-3 rounded-xl shadow hover:bg-green-800 transition duration-300">Shop Now</button>
+                        </a>
+                        <a href="http://">
+                            <button className="px-6 py-3 bg-white text-gray-700 border border-green-700 rounded-xl hover:bg-green-100 transition duration-300">Learn More</button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default Hero;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,9 @@
+import Hero from "../components/Hero";
+
 function Home(){
     return (
         <div>
+            <Hero />
             <h1>Home!!!! Page</h1>
         </div>
     )
